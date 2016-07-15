717880edc229b355c6bd3555e4dbd1b7ab7dd273
Geebz <alexandrveklich@gmail.com>
2016-07-15 22:09:58 +0300

    update journal,base,group

---
+++ students/views/forme.py
@@ -0,0 +1,7 @@
+check = {1,3,5,29,30}
+
+for i in range(31):
+    if i in check:
+        print 'wow'
+    else:
+        print i
--- students/views/journal.py
+++ students/views/journal.py
@@ -5,4 +5,21 @@ from django.http import HttpResponse
 
 
 def journal_list(request):
-    return render(request, 'students/journal.html',{})
+    journals = (
+        {
+            'id':1,
+            'name':u'Артюх Євген',
+            'checked':{1,10,15}
+        },
+        {
+            'id': 2,
+            'name': u'Андрій Ковтун',
+            'checked': {3, 4, 17,23}
+        },
+        {
+            'id': 3,
+            'name': u'Артюх Євген',
+            'checked': {1, 11, 13,19}
+        }
+    )
+    return render(request, 'students/journal.html',{'journals':journals})
