6c7c1199a573c1ea8dcfe8d9e4f1d0dcd3597073
Geebz <alexandrveklich@gmail.com>
2016-07-23 01:55:58 +0300

    add order_by in all columns

--- students/views/students.py
+++ students/views/students.py
@@ -3,16 +3,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from ..models import Student
 
+
 # Views for student
 def students_list(request):
     students = Student.objects.all()
-    return render(request,'students/students_list.html', {'students':students})
+    # order student list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('last_name', 'first_name', 'ticket', 'id'):
+        students = students.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            students = students.reverse()
+    else:
+        students = students.order_by('last_name')
+
+    return render(request, 'students/students_list.html', {'students': students})
+
 
 def students_add(request):
     return HttpResponse('<h1>Students Add Form</h1>')
 
+
 def students_edit(request, sid):
     return HttpResponse('<h1>Students Edit %s</h1>' % sid)
 
+
 def students_delete(request, sid):
     return HttpResponse('<h1>Students Delete %s</h1>' % sid)
+
